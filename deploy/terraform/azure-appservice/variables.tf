715fef2986b301ba167e4191ee09726fa12722cc
Aleksy <aleksypyrz@gmail.com>
2026-02-12 14:21:35 +1000

    sh scripts

---
+++ deploy/terraform/azure-appservice/variables.tf
@@ -0,0 +1,6 @@
+variable "project" { type = string }
+variable "environment" { type = string }
+variable "location" { type = string }
+variable "location_short" { type = string }
+variable "resource_group_name" { type = string }
+variable "app_name" { type = string }
